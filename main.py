283ce10d4dfc4dcbfa039c67d09390c8d3586ed4
LorisDeBiasi <loris.debiasi@gmail.com>
2018-12-07 00:00:34 +0100

    Polynomiale interpolation working
    changing/debuging the polynomiale interpolation to make it work (atleast for the test i did)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import sys
 
 # Interpolation
 class Interpolation:
@@ -19,7 +22,7 @@ class Interpolation:
 	b = 0
 	
 	#
-	point = [[],[]] # faire en sorte que point[y][index] et point[x][index]
+	point = {'x':[],'y':[]} # faire en sorte que point[y][index] et point[x][index]
 
 	#-----------------------------------------
 	# CONSTRUCTOR
@@ -36,91 +39,33 @@ class Interpolation:
 	#-----------------------------------------
 
 	@abstractmethod
-	def CreatePoint(self):
-		self.point = []
-		self.point.append([])
-		self.point.append([])
-		# calcule y=f(x) pour chaque les 'N' 'x' de 'a' à 'b'
-		for x in numpy.linspace(self.a, self.b, self.N):
-			self.point[0].append(x)
-			self.point[1].append(self.Function(x))
-
-	# f(x)
-	@abstractmethod
 	def Function(self, x):
-		return (4*x**3-3*x-4)/(5*x**2+x+1);
+		return 1/x#(4*x**3-3*x-4)/(5*x**2+x+1)
 
-	# formule de Newton
 	@abstractmethod
-	def InterpolationPolynomiale(self, x):	
-		result = self.point[1][0]  # y[x0]
-		for i in range(0, self.N-1):
-			tmp = 1
-			for xi in range(0, i+1):
-				print("xi",xi)
-				tmp *= (x - self.point[0][xi])
-			print(" ")
-			tmp*= self.Derivative(0, i+1)
-			result+=tmp
-		return result
-	'''
+	def CreatePoint(self):
+		for i in np.linspace(self.a, self.b, self.N):
+			self.point['x'].append(i)
+			self.point['y'].append(self.Function(i))
+
+	@abstractmethod
+	def DividedDifference(self, yPoint):
+		nbElement = len(yPoint)
+		if nbElement == 2:
+			return (yPoint[1] - yPoint[0]) / (self.point['x'][1] - self.point['x'][0])
+		else:
+			return (self.DividedDifference(yPoint[1:nbElement])) - self.DividedDifference(yPoint[0:nbElement-1]) / (self.point['x'][nbElement-1] - self.point['x'][0])
+
 	@abstractmethod
 	def InterpolationPolynomiale(self, x):
-		# y[x0]
-		result = self.point[1][0]
-
-		# Newton Formula
-		for i in range(1,self.N):
-			# Yeme différence divisé
-			param = self.point[:i]
-			otherY = self.Derivative(param)
-
-			# Generate all the (x - xN)
-			for j in range(0,i):
-				otherY *= (x - self.point[0][j])
-
-			# Add the current B to the final result
-			result += otherY
-
-		return result
-	'''
-	#@abstractmethod
-	#def B(self, index):
-	#	return (self.Derivative(index+2) - self.Derivative(index)) / (self.point[0][index+2]- self.point[0][index])
-
-	def DividedDifference(self, param):
-		nbElement = len(array)
-		for i in range(0,nbElement):
-			self.DividedDifference(self, param)
-
-	#takes i as first index
-	#takes n as nb of index
-	@abstractmethod
-	def Derivative(self, i, n):
-		result = 0.0
-		if i == n:
-			result = self.point[1][i]
-		elif n-1==0: 
-			result = (self.Derivative(i+1, n) - self.Derivative(i, n-1)) / float( self.point[0][n] - self.point[0][i])
-		else:
-			#print(" i=",i,"n=",n)
-			result = float((((n-1)**self.Derivative(i+1, n)) - ((n-1)**self.Derivative(i, n-1)))) / float(( self.point[0][n] - self.point[0][i]))
-		return result
-	#def Derivative(self, indexs):
-		#nbIndex = len(indexs)
-		#result = 0.0;
-		#if nbIndex == 1:
-		#	result = self.point[1][index[0]]
-		#else:
-		# CERTAINEMENT PROBLEME DANS LES INDEX : indexs[1:]
-		#result = (((nbIndex-1)**self.Derivative(indexs[1:])) - ((nbIndex-1)**self.Derivative(nbIndex[:-1]))) / (self.point[0][index[nbIndex-1]] - self.point[0][0])
+		pass	
 
 	@abstractmethod
-	def InterpolationContinue(self):
+	def InterpolationContinue(self, x):
 		pass
 
 	@abstractmethod
-	def InterpolationSpline(self):
+	def InterpolationSpline(self, x):
 		pass
 
 	@abstractmethod
@@ -130,25 +75,47 @@ class Interpolation:
 # InterpolationUnidimensionnele
 class InterpolationUnidimensionnele(Interpolation):
 
-	#def InterpolationPolynomiale(self):
-	#	pass
+	#
+	def InterpolationPolynomiale(self, x):
+		# y[x0]
+		result = self.point['y'][0]
 
-	def InterpolationContinue(self):
-		pass
+		# Newton Formula
+		for i in range(1,self.N):
+			# Y'th divided difference
+			otherY = self.DividedDifference(self.point['y'][:i+1])
 
-	def InterpolationSpline(self):
+			# Generate all the (x - xN)
+			for j in range(0,i):
+				otherY *= (x - self.point['x'][j])
+
+			# Add the current B to the final result
+			result += otherY
+
+		return result
+
+	#
+	def InterpolationContinue(self, x):
+		for i in np.linspace(self.a, self.b, self.N):
+			x = a + (b-a/N) * i
+
+	#
+	def InterpolationSpline(self, x):
 		pass
 
 # InterpolationBidimensionnele
 class InterpolationBidimensionnele(Interpolation):
 
-	def InterpolationPolynomiale(self):
+	#
+	def InterpolationPolynomiale(self, x):
 		pass
 
-	def InterpolationContinue(self):
+	#
+	def InterpolationContinue(self, x):
 		pass
 
-	def InterpolationSpline(self):
+	#
+	def InterpolationSpline(self, x):
 		pass
 
 
@@ -158,17 +125,19 @@ def main():
 
 	x = 5
 
-	#print(test.Function(x))
-	#print(test.point[0][1:])
-	print(test.point)
-	#t = ( test.point[0][1] - test.point[0][0])
-	#print("result poli",t)
-	print(test.InterpolationPolynomiale(x))
-	#d = float((((0)**test.Derivative(1, 1)) - ((0)**test.Derivative(0, 0))))
-	#print("deriv",d)
-	#print(test.point[1][0])
-	#print(test.CreatePoint)
+	#print(test.point['x'])
+	#print(test.point['y'])
+
+	#plt.plot(test.point[0],test.point[1])
+	#plt.xlabel("$x$")
+	#plt.ylabel("$y$")
+	#plt.grid(True)
+	#plt.show()
+
+	print("Function    : {}".format(test.Function(x)))
+	print("Polynomiale : {}".format(test.InterpolationPolynomiale(x)))
 
 
 if __name__ == "__main__":
+	#sys.setrecursionlimit(100000)
 	main()
